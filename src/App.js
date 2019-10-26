91e2d96b62ee7d70ed5c9dc372805df503426110
Willie Edwards <gammaray360@gmail.com>
2019-10-26 11:04:56 -0700

    changed images, added gif background to header, other styletweaks

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
   gameOver = () => {
     if (this.state.score > this.state.highScore) {
-      this.setState({highScore: this.state.score}, function() {
+      this.setState({ highScore: this.state.score }, function () {
         console.log(this.state.highScore);
       });
     }
@@ -22,39 +22,41 @@ class App extends Component {
       card.count = 0;
     });
     alert(`GAME OVER \nYOUR SCORE: ${this.state.score}`);
-    this.setState({score: 0});
+    this.setState({ score: 0 });
     return true;
   }
 
   clickCount = id => {
     this.state.cards.find((o, i) => {
       if (o.id === id) {
-        if (cards[i].count === 0){
+        if (cards[i].count === 0) {
           cards[i].count = cards[i].count + 1;
-          this.setState({score : this.state.score + 1}, function(){
+          this.setState({ score: this.state.score + 1 }, function () {
           });
           this.state.cards.sort(() => Math.random() - 0.5)
-          return true; 
+          return true;
         } else {
           this.gameOver();
         }
-      } 
+      }
     });
   }
   render() {
     return (
       <div className="main">
-      <Wrapper>
-        <Header score={this.state.score} highScore={this.state.highScore}>Memory Lab</Header>
-        {this.state.cards.map(card => (
-          <Card
-            clickCount={this.clickCount}
-            id={card.id}
-            key={card.id}
-            image={card.image}
-          />
-        ))}
-      </Wrapper>
+        <Wrapper>
+          <Header score={this.state.score} highScore={this.state.highScore}>
+            Memory Lab
+          </Header>
+          {this.state.cards.map(card => (
+            <Card
+              clickCount={this.clickCount}
+              id={card.id}
+              key={card.id}
+              image={card.image}
+            />
+          ))}
+        </Wrapper>
       </div>
     );
   }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,13 +2,11 @@ import React from "react";
 import "./Header.css";
 
 const Header = props => (
-  <div className="header text-center">{props.children}
-    <div className="jumbotron">
-      Current Score: {props.score} 
-      <br/>
-      Highest Score: {props.highScore}
-    </div>
+  <div className="header text-center">
+    <h1>{props.children}</h1>
     <div className="scores">
+      <h2>Current Score: {props.score}</h2>
+      <h2>Highest Score: {props.highScore}</h2>
     </div>
   </div>
 );
