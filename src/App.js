2466a7dcff5815c2327e8034a5b3f746b14a5708
Willie Edwards <gammaray360@gmail.com>
2019-10-26 14:19:38 -0700

    rewrote game logic to account for win condition and user alerts

--- src/App.js
+++ src/App.js
@@ -2,62 +2,126 @@ import React, { Component } from "react";
 import Card from "./components/Card/Card";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Header from "./components/Header/Header";
-import cards from "./cards.json";
+import card from "./characters.json";
 import "./App.css";
 
+let currentScore = 0;
+let highScore = 0;
+let clickAlert = "Click an image to increase your score, but don't click the same image twice.";
+
 class App extends Component {
+
+  // Setting this.state.card to the card json array
   state = {
-    cards,
-    score: 0,
-    highScore: 0
+    card,
+    currentScore,
+    highScore,
+    clickAlert
   };
 
-  gameOver = () => {
-    if (this.state.score > this.state.highScore) {
-      this.setState({ highScore: this.state.score }, function () {
-        console.log(this.state.highScore);
-      });
-    }
-    this.state.cards.forEach(card => {
-      card.count = 0;
-    });
-    alert(`GAME OVER \nYOUR SCORE: ${this.state.score}`);
-    this.setState({ score: 0 });
-    return true;
-  }
+  setClicked = id => {
 
-  clickCount = id => {
-    this.state.cards.find((o, i) => {
-      if (o.id === id) {
-        if (cards[i].count === 0) {
-          cards[i].count = cards[i].count + 1;
-          this.setState({ score: this.state.score + 1 }, function () {
-          });
-          this.state.cards.sort(() => Math.random() - 0.5)
-          return true;
-        } else {
-          this.gameOver();
-        }
+    // Make a copy of the state card array to work with
+    const card = this.state.card;
+
+    // Filter for the clicked match
+    const clickedMatch = card.filter(match => match.id === id);
+
+    // If an image has already been clicked, render losing alert
+    if (clickedMatch[0].clicked) {
+
+      console.log("Current Score: " + currentScore);
+      console.log("Highest Score: " + highScore);
+
+      currentScore = 0;
+      clickAlert = "All the courage in the world cannot alter fact. You lose!"
+
+      for (let i = 0; i < card.length; i++) {
+        card[i].clicked = false;
       }
-    });
-  }
+
+      this.setState({ clickAlert });
+      this.setState({ currentScore });
+      this.setState({ card });
+
+      // If an image hasn't been clicked, and high score hasn't been reached
+    } else if (currentScore < 11) {
+
+      // Set the value to true
+      clickedMatch[0].clicked = true;
+
+      // Add to the current score counter
+      currentScore++;
+
+      clickAlert = "You know what's real. Choose another image.";
+
+      if (currentScore > highScore) {
+        highScore = currentScore;
+        this.setState({ highScore });
+      }
+
+      // Shuffle the array to be rendered in a random order
+      card.sort(function (a, b) { return 0.5 - Math.random() });
+
+      // Set this.state.card equal to the new card array
+      this.setState({ card });
+      this.setState({ currentScore });
+      this.setState({ clickAlert });
+    } else {
+
+      // Set its value to true
+      clickedMatch[0].clicked = true;
+
+      // restart the guess counter
+      currentScore = 0;
+
+      // If high score reaches 12, render winning alert
+      clickAlert = "All the best memories are yours. You Win!";
+      highScore = 12;
+      this.setState({ highScore });
+
+      for (let i = 0; i < card.length; i++) {
+        card[i].clicked = false;
+      }
+
+      // Shuffle the array to be rendered in a random order
+      card.sort(function (a, b) { return 0.5 - Math.random() });
+
+      // Set this.state.card equal to the new card array
+      this.setState({ card });
+      this.setState({ currentScore });
+      this.setState({ clickAlert });
+
+    }
+  };
+
   render() {
     return (
-      <div className="main">
-        <Wrapper>
-          <Header score={this.state.score} highScore={this.state.highScore}>
-            Memory Lab
-          </Header>
-          {this.state.cards.map(card => (
-            <Card
-              clickCount={this.clickCount}
-              id={card.id}
-              key={card.id}
-              image={card.image}
-            />
-          ))}
-        </Wrapper>
-      </div>
+      <Wrapper>
+
+        <Header>
+          Memory Lab  
+          <h2>
+            Current Score: {this.state.currentScore}
+            <br />
+            Highest Score: {this.state.highScore}
+          </h2>
+          
+          <h3>
+            {this.state.clickAlert}
+          </h3>
+        </Header>
+
+            {this.state.card.map(match => (
+              <Card
+                setClicked={this.setClicked}
+                id={match.id}
+                key={match.id}
+                image={match.image}
+              />
+            ))}
+            
+      </Wrapper>
     );
   }
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,6 @@ import "./Header.css";
 const Header = props => (
   <div className="header text-center">
     <h1>{props.children}</h1>
-    <div className="scores">
-      <h2>Current Score: {props.score}</h2>
-      <h2>Highest Score: {props.highScore}</h2>
-    </div>
   </div>
 );
 
